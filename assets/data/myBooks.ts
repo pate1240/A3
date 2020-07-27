6a9c909ed874b142a48ac4d4a732328945149e77
pate1240 <47011189+pate1240@users.noreply.github.com>
2020-07-27 19:05:57 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,8 @@
+import { Book } from "../../app/bookInfo";
+
+export const MYBOOKS : Book[] = [
+    {BookName: "Elon Musk", Author: "Ashlee Vange", Genre: "Biography", Year: 2015, Picture: "elon.jpg"},
+    {BookName: "The Lean Startup", Author: "Eric Ries", Genre: "Business", Year: 2011, Picture: "tls.jpg"},
+    {BookName: "Zero To One", Author: "Peter Thiel, Blake Masters", Genre: "Business", Year: 2014, Picture: "zto.jpg"},
+    {BookName: "Welcome to the Future Which Is Mine", Author: "Elon Musk, Scott Dikkers", Genre: "Humour, Parody", Year: 2018, Picture: "elonmusk.jpg"}
+];
